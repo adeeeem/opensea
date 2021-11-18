714b15e602635286144235dcd2a0351d1a7fb6d3
adeeeem <adeem.zaki@scytalelabs.com>
2021-11-19 00:41:04 +0500

    he

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import App from "./app/containers/App/Application";
+import axios from "axios";
 
+// axios.defaults.withCredentials = true;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (process.env.REACT_APP_BACKEND_SERVER_ADDRESS)
+  axios.defaults.baseURL = `${process.env.REACT_APP_BACKEND_SERVER_ADDRESS}`;
+// else axios.defaults.baseURL = `http://localhost:3000`;
+else axios.defaults.baseURL = `https://robotdropdemo.herokuapp.com/`;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// // import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import "./index.css";
+// import * as serviceWorker from "./serviceWorker";
+// import { BrowserRouter } from "react-router-dom";
+// // import ErrorScreen from "./components/ErrorScreen";
+// // import AdminDashboard from "./components/AdminDashboard";
+// import App from "./App";
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
